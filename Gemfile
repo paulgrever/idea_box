3c62fa85090664eeab7f689886d6ce7f5d58f2d2
Paul Grever <paulgrever@gmail.com>
2015-03-05 16:02:00 -0700

    set up idea model with all passing tests and validations

--- Gemfile
+++ Gemfile
@@ -39,6 +39,7 @@ group :development, :test do
   gem 'launchy'
   gem 'rspec-rails'
   gem 'pry'
+  gem 'database_cleaner'
 
   # Access an IRB console on exception pages or by using <%= console %> in views
   gem 'web-console', '~> 2.0'
--- spec/models/idea_spec.rb
+++ spec/models/idea_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Idea, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:idea) do 
+    Idea.new(title: "Dream", body: "TEXT")
+  end
+
+  it "exits" do
+    expect(idea).to be_valid
+  end
+
+  it "it has valid attributes" do
+    expect(idea.title).to eq("Dream")
+    expect(idea.body).to eq("TEXT")
+  end
+
+  it "is invalid without a title" do
+    idea.title = nil
+    expect(idea).to_not be_valid
+  end
+
+  it "is invalid without a body" do
+    idea.body = nil
+    expect(idea).to_not be_valid
+  end
+
+  it "has an id when valid and saved" do
+    idea1 =Idea.create(title: "Dream", body: "TEXT")
+    expect(idea1.id).to eq(1)
+  end
 end
